d1a7997ae7140155c9e746f9b0a0fda5c096697e
kenashah <kena@ualberta.ca>
2017-09-25 16:54:00 -0600

    Second commit

---
+++ kena-CountBook/app/src/main/java/com/example/kena_countbook/Counter.java
@@ -0,0 +1,13 @@
+package com.example.kena_countbook;
+
+import java.util.Date;
+
+/**
+ * Created by kenajshah on 2017-09-25.
+ */
+
+public class Counter {
+    private String name, comment;
+    private Date date;
+    private Integer current_value, initial_value;
+}
