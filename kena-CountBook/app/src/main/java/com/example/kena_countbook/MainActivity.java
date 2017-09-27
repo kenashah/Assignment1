b56bf052c00c7e63ba36767eb2e0ab3716b822ef
kenashah <kena@ualberta.ca>
2017-09-26 22:39:12 -0600

    Fourth commit

--- kena-CountBook/app/src/main/java/com/example/kena_countbook/MainActivity.java
+++ kena-CountBook/app/src/main/java/com/example/kena_countbook/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.kena_countbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.kena_countbook.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,7 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void NewAndroidButtonClicked() {
-        Counter counter = new Counter();
+
+    public void NewCounterButtonClicked(View view) {
+        // Do something in response to button
+        // display the Counter fields, which will take the inputs to create a new counter
+        Intent intent = new Intent(this, CreateCounter.class);
+        startActivity(intent);
     }
 }
